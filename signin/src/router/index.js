f8f467f1009a3415409ba999ac08d69484b61432
Vinol Fernandes <Vinolfernandes20@gmail.com>
2022-07-19 19:47:22 +0530

    basic working of sign -in

--- signin/src/router/index.js
+++ signin/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/HomeView.vue'
-
+// import { auth } from 'firebase/auth'
 const routes = [
   {
     path: '/',
@@ -30,5 +30,17 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+// router.beforeEach((to,from,next)=>{                       //preserves authentication after routing to home page
+//   if(to.path === '/login' && auth.currentUser){
+//     next('/')
+//     return;
+//   }
+//   if(to.matched.some(record=> record.meta.requiresAuth)&& !auth.currentUser){
+//     next('/login')
+//     return;
+//   }
+//   next();
+// })
+
 
 export default router
